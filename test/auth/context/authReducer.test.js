34867695509d437c1c3b814628788d6b0f0a2cbf
Simon <simonbustamante21@gmail.com>
2023-04-02 22:13:40 -0400

    primeras pruebas a authReducer, a la aplicacion heroes

---
+++ test/auth/context/authReducer.test.js
@@ -0,0 +1,53 @@
+import { authReducer } from "../../../src/auth/context/authReducer";
+import { types } from "../../../src/auth/types/types";
+
+describe('Pruebas en authReducer', () => {
+
+    test('debe de retornar el estado por defecto ', () => {
+        
+        const state = authReducer({ logged: false}, {})
+
+        expect(state).toEqual({logged: false});
+
+    });
+
+    test('debe de (login) llamar el login autenticar y establecer el user', () => {
+        
+        const action = {
+            type: types.login,
+            payload: {
+                name: 'Iván',
+                id: '123'
+            }
+        }
+
+        const state = authReducer({ logged: false}, action );
+
+        expect(state).toEqual({
+            logged: true,
+            user: action.payload
+        })
+    });
+
+    test('debe de (logout) borrar el name del usuario y el logged en false ', () => {
+        
+        const state = {
+            logged: true,
+            user: {
+                id:'123',
+                name: 'Iván'
+            }
+        };
+
+        const action = {
+            type: types.logout
+        }
+
+        const newState = authReducer( state, action );
+
+        expect( newState ).toEqual({ logged: false });
+
+    });
+    
+
+});
